64eff7aaa6311f6afcc0c1507fe7079a5b4ab31f
bporignaux <baptiste.prx8@gmail.com>
2026-04-30 14:08:49 +0200

    vla les changements encore

--- routes/api.php
+++ routes/api.php
@@ -3,9 +3,14 @@
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\Api\TestApiController;
+use App\Http\Controllers\Api\AuthController;
+
+Route::post('/login', [AuthController::class, 'login']);
 
 Route::get('/releves', [TestApiController::class, 'index']);
 
 Route::get('/user', function (Request $request) {
     return $request->user();
 })->middleware('auth:sanctum');
+
+Route::post('/login', [AuthController::class, 'login']);
